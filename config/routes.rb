ba9af73220a29031509486ba3a9beae3712c8edf
Jules <rigomontj@gmail.com>
2018-06-02 17:40:17 +0200

    added views and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'onboardings/index'
+
   devise_for :sellers
   devise_for :users
+  root 'onboardings#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
