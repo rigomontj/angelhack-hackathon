7322e9c88c0f47fca38afbbf547de6043f2fa774
Antonin <antonin.siguierfr@gmail.com>
2018-06-02 17:26:56 +0200

    migrated table product

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20180602152330) do
 
+  create_table "products", force: :cascade do |t|
+    t.text "name"
+    t.integer "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sellers", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
