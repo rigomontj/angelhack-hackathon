836b18230a8472f6ef7289e20ee16e8d354fe18b
Antonin <antonin.siguierfr@gmail.com>
2018-06-02 17:35:47 +0200

    migrated transaction table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602152330) do
+ActiveRecord::Schema.define(version: 20180602153440) do
 
   create_table "products", force: :cascade do |t|
     t.text "name"
@@ -35,6 +35,17 @@ ActiveRecord::Schema.define(version: 20180602152330) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_sellers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_sellers_on_reset_password_token", unique: true
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.text "to"
+    t.text "from"
+    t.text "product_name"
+    t.integer "unit_price"
+    t.integer "quantity"
+    t.integer "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
